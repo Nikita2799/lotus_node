86e7665d39d795847dfa7b6f451b6d27cafc8c4f
Nikita2799 <filatovnikita27@gmail.com>
2021-11-26 17:30:29 +0200

    fix change pass

--- src/databaseApi/api/UserApi.ts
+++ src/databaseApi/api/UserApi.ts
@@ -1,8 +1,33 @@
 import { Sequelize } from "sequelize";
 import { User } from "../model/User";
+import bcrypt from "bcryptjs";
 
 export class UserApi {
   constructor(private readonly connection: Sequelize) {}
+
+  public async changePassword(
+    id: any,
+    oldPassword: string,
+    newPassword: string
+  ) {
+    const result = await User.findOne({
+      where: { id: id },
+      attributes: ["password"],
+      raw: false,
+    });
+    const hashPassword = await bcrypt.hash(oldPassword, 10);
+    const isMatch = await bcrypt.compare(
+      hashPassword,
+      result?.getDataValue("password")
+    );
+
+    if (!isMatch) throw new Error("incorrect pass");
+
+    const hashNewPassword = await bcrypt.hash(newPassword, 10);
+    await User.update({ password: hashNewPassword }, { where: { id: id } });
+
+    return result;
+  }
 
   public async get(id: any) {
     const result = await User.findOne({
